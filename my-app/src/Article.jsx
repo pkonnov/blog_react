f6a31d5080d47d9a89c72f2b8e4605857607e85e
ызукьызфсу <4itonada@gmail.com>
2018-05-10 16:41:53 +0400

    add list articles

---
+++ my-app/src/Article.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+
+export default class Article extends Component{
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+
+  }
+  render(){
+    const {article} = this.props
+    const {isOpen} = this.state
+    return(
+      <div>
+        <h3>{article.title}</h3>
+        <button onClick={this.toogleOpen}>{isOpen ? 'close' : 'open'}</button>
+        {this.getBody()}
+      </div>
+    )
+  }
+
+  getBody(){ //view and close text article
+    if (!this.state.isOpen) return null
+    const {article} = this.props
+    return <section>{article.text}</section>
+  }
+
+  toogleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+}
